c5cca653e3217dba9d931f41af8506d9f0275acb
Abdullah Aleem <aaleem.bscs21seecs@seecs.edu.pk>
2024-07-19 01:40:21 +0500

    refactoringPlacesForm

---
+++ client/src/UploadPhoto.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import React,{useState} from 'react'
+function UploadPhoto({addedPhoto,setAddedPhoto}) {
+    const [photoLink,setPhotoLink]=useState('');
+
+    async function addPhotoByLink(ev){
+        ev.preventDefault();
+       axios.post('/uploadByToken',{link:photoLink}).then(data=>{
+        setAddedPhoto(prev =>{
+            return [...prev,data.data];
+        })
+       }).catch(err=>{
+        console.log(err)
+       })
+       setPhotoLink('')
+       console.log(addedPhoto)
+    }
+    function uploadPhoto(ev){
+        const files=ev.target.files;
+        const data = new FormData();
+        for (let i=0;i<files.length;i++){
+
+            data.append('photos',files[i]);
+        }
+        console.log(data)
+        axios.post('/upload',data,{
+            headers:{ 'Content-type':'multipart/form-data'}
+        }).then(response=>{
+            const {data}=response;
+            console.log(data)
+            setAddedPhoto(prev =>{
+                return [...prev,data];
+            })
+
+        })
+    }
+    return (
+        <div >
+            <div className='flex gap-2'>
+                <input type="text" value={photoLink} onChange={e => setPhotoLink(e.target.value)} placeholder={'add using link.....'} />
+                <button onClick={addPhotoByLink} className='bg-gray-200 rounded-2xl px-4' >Add&nbsp;photos</button>
+            </div>
+            <div className='grid grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-6'>
+                {addedPhoto.length > 0 && addedPhoto.map(link => (
+
+                    <div className='h-32 '>
+                        <img className=' rounded-2xl h-full w-full object-cover ' src={'http://localhost:4000/uploads/' + link} />
+                    </div>
+                ))}
+                <label className='h-32 cursor-pointer flex justify-center gap-1 border bg-transparent rounded-2xl p-2 items-center text-2xl text-gray-500'>
+                    <input type="file" multiple={true} className='hidden ' onChange={uploadPhoto} />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
+                    </svg>
+                    Upload
+                </label>
+            </div></div>
+    )
+}
+
+export default UploadPhoto
--- client/src/pages/Places.jsx
+++ client/src/pages/Places.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 import { Link, Navigate, useParams } from 'react-router-dom'
 import Perks from '../Perks';
 import axios from 'axios';
+import UploadPhoto from '../UploadPhoto';
 function Places() {
     const { action } = useParams();
     const [title,setTitle]=useState('');
     const [address,setAdress]=useState('');
     const [addedPhoto,setAddedPhoto]=useState([]);
-    const [photoLink,setPhotoLink]=useState('');
     const [description,setDescription]=useState('');
     const [perks,setPerks]=useState([]);
     const [extraInfo,setExtraInfo]=useState('');
@@ -33,37 +33,7 @@ function Places() {
             </>
         )
     }
-    async function addPhotoByLink(ev){
-        ev.preventDefault();
-       axios.post('/uploadByToken',{link:photoLink}).then(data=>{
-        setAddedPhoto(prev =>{
-            return [...prev,data.data];
-        })
-       }).catch(err=>{
-        console.log(err)
-       })
-       setPhotoLink('')
-       console.log(addedPhoto)
-    }
-    function uploadPhoto(ev){
-        const files=ev.target.files;
-        const data = new FormData();
-        for (let i=0;i<files.length;i++){
-
-            data.append('photos',files[i]);
-        }
-        console.log(data)
-        axios.post('/upload',data,{
-            headers:{ 'Content-type':'multipart/form-data'}
-        }).then(response=>{
-            const {data}=response;
-            console.log(data)
-            setAddedPhoto(prev =>{
-                return [...prev,data];
-            })
-
-        })
-    }
+    
 
     if (redirect){
         return (
@@ -92,23 +62,7 @@ function Places() {
                             <input type='text' value={address} onChange={e=> setAdress(e.target.value)} placeholder='address' />
                             {preinput('Photos','more===better')}               
                             <div className='flex gap-2'>
-                                <input type="text" value={photoLink} onChange={e=> setPhotoLink(e.target.value)} placeholder={'add using link.....'} />
-                                <button onClick={addPhotoByLink} className='bg-gray-200 rounded-2xl px-4' >Add&nbsp;photos</button>
-                            </div>
-                            <div className='grid grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-6'>
-                                {addedPhoto.length>0 && addedPhoto.map(link => (
-                                    
-                                     <div className='h-32'>
-                                        <img className=' rounded-2xl w-full object-cover' src={'http://localhost:4000/uploads/'+link}  />
-                                    </div>
-))}
-                                <label className='h-32 cursor-pointer flex justify-center gap-1 border bg-transparent rounded-2xl p-2 items-center text-2xl text-gray-500'>
-                                    <input type="file"multiple={true} className='hidden ' onChange={uploadPhoto}/>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-8 h-8">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
-                                    </svg>
-                                    Upload
-                                </label>
+                            <UploadPhoto addedPhoto={addedPhoto} setAddedPhoto={setAddedPhoto}/>
                             </div>
                             {preinput('Description','Description of your place')}
                             <textarea className='' value={description} onChange={e=> setDescription(e.target.value)} />
