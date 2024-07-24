218f68ae3be7313223b9216740e29ad8b9557a29
Abdullah Aleem <aaleem.bscs21seecs@seecs.edu.pk>
2024-07-25 02:20:42 +0500

    AddRemovePhoto

--- client/src/UploadPhoto.jsx
+++ client/src/UploadPhoto.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { set } from 'mongoose';
+
 import React, { useState } from 'react'
 function UploadPhoto({ addedPhoto, setAddedPhoto }) {
     const [photoLink, setPhotoLink] = useState('');
@@ -15,7 +15,7 @@ function UploadPhoto({ addedPhoto, setAddedPhoto }) {
             console.log(err)
         })
         setPhotoLink('')
-        
+
     }
     function uploadPhoto(ev) {
         const files = ev.target.files;
@@ -24,27 +24,30 @@ function UploadPhoto({ addedPhoto, setAddedPhoto }) {
 
             data.append('photos', files[i]);
         }
-       
+
         axios.post('/upload', data, {
             headers: { 'Content-type': 'multipart/form-data' }
         }).then(response => {
             const { data } = response;
-            console.log("this is data",data)
-            
+            console.log("this is data", data)
+
             setAddedPhoto(prev => {
-                
+
                 return prev.concat(data);
             })
 
         })
     }
-    const makePrimary=(ev)=>{
+    const makePrimary = (ev) => {
         ev.preventDefault();
-        const link=ev.target.src.split('/').pop();
+        const link = ev.target.src.split('/').pop();
         console.log(link)
         console.log(addedPhoto)
-        setAddedPhoto([link,...addedPhoto.filter(photo=>photo!==link)]);
+        setAddedPhoto([link, ...addedPhoto.filter(photo => photo !== link)]);
 
+    }
+    const removePhoto=(link)=>{
+        setAddedPhoto(prev=>prev.filter(photo=>photo!==link))
     }
     return (
         <div className='w-full' >
@@ -55,8 +58,15 @@ function UploadPhoto({ addedPhoto, setAddedPhoto }) {
             <div className='grid grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-6'>
                 {addedPhoto.length > 0 && addedPhoto.map(link => (
 
-                    <div className='h-32 ' key={link}>
-                        <img className=' rounded-2xl h-full w-full object-cover cursor-pointer ' key={link} src={'http://localhost:4000/uploads/' + link}  onClick={makePrimary}/>
+                    <div className='h-32 flex relative' key={link}>
+                        <img className=' rounded-2xl h-full w-full object-cover cursor-pointer ' key={link} src={'http://localhost:4000/uploads/' + link} onClick={makePrimary} />
+                        <button onClick={()=>{removePhoto(link)}} className='absolute bottom-1 right-1 bg-black text-white py-1 px-2 bg-opacity-50 rounded-xl'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+
+                        </button>
+
                     </div>
                 ))}
                 <label className='h-32 cursor-pointer flex justify-center gap-1 border bg-transparent rounded-2xl p-2 items-center text-2xl text-gray-500'>
